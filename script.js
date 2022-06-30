9058fb1d642ec3b24675b07909d02ba829dee08a
Socootra <mriziq54@gmail.com>
2022-06-30 18:30:04 +0700

    adding title on home page and some animations on destinations page

--- script.js
+++ script.js
@@ -20,20 +20,26 @@ $(document).ready(function () {
     };
 
     img[0].onclick = ()=>{
+        $(".wrapper").css({"background-color": "transparent", 'margin':  '20px 50px'});
         bg.style.backgroundImage = "url('assets/image/anjungan.jpg')";
         title.innerHTML = "Anjungan";
         explain.innerHTML = "Spot wisata di TMII yang tak boleh dilewatkan adalah anjungan. Di sana terdapat banyak anjungan atau replika rumah daerah mulai dari Aceh hingga Papua. Pengunjung bisa berfoto di anjungan rumah adat itu."
         foregroundColor("white");
     };img[1].onclick = ()=>{
+        $(".wrapper").css({"background-color": "transparent", 'margin':  '20px 50px'});
         bg.style.backgroundImage = "url('assets/image/ppiptek.JPG')";
+        title.innerHTML = "PP-IPTEK";
         explain.innerHTML = "Bagi pengunjung yang ingin wisata edukasi, di TMII juga ada PP-IPTEK merupakan Pusat Peragaan Ilmu Pengetahuan dan Teknologi. Di sana pengunjung bisa mencoba wahana listrik, magnet, laboratorium komputer, hingga patung dinosaurus."
         foregroundColor("white");
     };img[2].onclick = ()=>{
+        $(".wrapper").css({"background-color": "transparent", 'margin':  '20px 50px'});
         bg.style.backgroundImage = "url('assets/image/Jakarta-TMII_1920x1080px-1030x579.jpg')";
         title.innerHTML = "Keong Mas";
         explain.innerHTML = "Taman Legenda Keong Emas ini menawarkan sejumlah wahana taman bermain yang seru dan menarik. Ada Petualangan Dinosaurus, Mata Legenda, Ulat Selur, Bajak Laut, Mobil Tanjak, Kereta Beos, Baby Dino, Perang Galaksi, dan Nirwata Kisar."
         foregroundColor("black");
     };img[3].onclick = ()=>{
+        $(".wrapper").css({'background-color': '#F2F1ED'});
+        $(".wrapper").animate({margin: '20px 0px'});
         bg.style.backgroundImage = "url('assets/image/Taman.jpg')";
         title.innerHTML = "Perahu Angsa";
         explain.innerHTML = "Perahu angsa digunakan untuk menjelajahi miniatur pulau-pulau dan berkeliling danau. Tarif yang dikenakan adalah 7.500 Rupiah per orang."
